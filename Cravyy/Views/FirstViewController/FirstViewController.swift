f66074c4c1084d401e4c361586a29c2b6fa62673
Victor Alvarenga <alvarengavictor73@gmail.com>
2021-09-14 02:20:52 +0100

    finished sign up screen

--- Cravyy/Views/FirstViewController/FirstViewController.swift
+++ Cravyy/Views/FirstViewController/FirstViewController.swift
@@ -10,19 +10,47 @@ import UIKit
 class FirstViewController: UIViewController {
     
     @IBOutlet weak var WelcomeLabel: UILabel!
-    
     @IBOutlet weak var descriptionLabel: UILabel!
-    
-    
 
+    
+    var signUpVC :SignUpViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    loadWelcomeLabel()
+         signUpVC = storyboard?.instantiateViewController(identifier: "SignUpVC") as? SignUpViewController
         
+    
     }
     
     @IBAction func SignupWithGoogleTapped(_ sender: Any) {
     }
     
+    @IBAction func signUptapped(_ sender: Any) {
+        if signUpVC != nil{
+            present(signUpVC!, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    
+    //MARK:-Methods to Load Labels
+    
+    func loadWelcomeLabel (){
+       
+        WelcomeLabel.text=""
+              let titleText="Welcome🐒"
+                var charIndex = 0.0
+              for letter in titleText{
+                   Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
+                      self.WelcomeLabel.text?.append(letter)
+                   }
+                 charIndex+=1
+              }
+    }
+   
   
 }
+
+
---
+++ Cravyy/Views/SignUp and SignIn/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Cravyy
+//
+//  Created by VD on 14/09/2021.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}

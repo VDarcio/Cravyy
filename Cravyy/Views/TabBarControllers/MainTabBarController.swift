49f79d4f171ba873e3b21ad72aa94a475c2d693c
Victor Alvarenga <alvarengavictor73@gmail.com>
2021-09-16 02:16:19 +0100

    Started TabBar UI

---
+++ Cravyy/Views/TabBarControllers/MainTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabBarController.swift
+//  Cravyy
+//
+//  Created by VD on 16/09/2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
---
+++ Cravyy/Views/TabBarControllers/MapKitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapKitViewController.swift
+//  Cravyy
+//
+//  Created by VD on 16/09/2021.
+//
+
+import UIKit
+
+class MapKitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
